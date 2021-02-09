ee31d8bc017b4b347697400edfd61bbf36f644e0
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-02-09 12:13:23 -0600

    cleaning up config

--- create-beehive.sh
+++ create-beehive.sh
@@ -1,26 +1,110 @@
 #!/bin/bash
 
-setup_rabbitmq_user() {
-    user="$1"
-    password="$2"
-    kubectl exec svc/rabbitmq -- bash -c "
-rabbitmqctl add_user \"$user\" \"$password\" || rabbitmqctl change_password \"$user\" \"$password\"
-rabbitmqctl set_permissions \"$user\" \".*\" \".*\" \".*\"
-"
+files_exist() {
+    for f in $*; do
+        if ! test -e "$f"; then
+            return 1
+        fi
+    done
 }
 
-kubectl create -f rabbitmq.yaml
+# using a self-signed certificate for all public tls endpoints for now.
+if ! files_exist cacert.pem cert.pem key.pem; then
+    openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out cert.pem -subj "/CN=beehive"
+    cp cert.pem cacert.pem
+fi
 
-echo "waiting for rabbitmq"
+# generate rabbitmq definitions file. this only creates / updates the config of things
+# in this definitions file - other preexisting resources are not affected.
+cat <<EOF > definitions.json
+{
+    "users": [
+        {
+            "name": "admin",
+            "password": "admin",
+            "tags": "administrator",
+            "limits": {}
+        },
+        {
+            "name": "service",
+            "password": "service",
+            "tags": "",
+            "limits": {}
+        }
+    ],
+    "vhosts": [
+        {
+            "name": "/"
+        }
+    ],
+    "permissions": [
+        {
+            "user": "admin",
+            "vhost": "/",
+            "configure": ".*",
+            "write": ".*",
+            "read": ".*"
+        },
+        {
+            "user": "service",
+            "vhost": "/",
+            "configure": "^$",
+            "write": ".*",
+            "read": ".*"
+        }
+    ],
+    "topic_permissions": [],
+    "parameters": [],
+    "policies": [],
+    "queues": [],
+    "exchanges": [
+        {
+            "name": "waggle.msg",
+            "vhost": "/",
+            "type": "topic",
+            "durable": true,
+            "auto_delete": false,
+            "internal": false,
+            "arguments": {}
+        }
+    ],
+    "bindings": []
+}
+EOF
 
-while ! kubectl exec svc/rabbitmq -- rabbitmqctl await_startup --timeout 300; do
-    sleep 1
-done
+cat <<EOF > rabbitmq.conf
+load_definitions = /etc/rabbitmq/definitions.json
 
-setup_rabbitmq_user admin admin
-setup_rabbitmq_user service service
+ssl_options.cacertfile           = /etc/rabbitmq/cacert.pem
+ssl_options.certfile             = /etc/rabbitmq/cert.pem
+ssl_options.keyfile              = /etc/rabbitmq/key.pem
+ssl_options.fail_if_no_peer_cert = false
+# ssl_options.verify               = verify_peer
 
-# TODO extract config management as we get there
+management.ssl.cacertfile = /etc/rabbitmq/cacert.pem
+management.ssl.certfile   = /etc/rabbitmq/cert.pem
+management.ssl.keyfile    = /etc/rabbitmq/key.pem
+EOF
+
+kubectl create secret generic rabbitmq-config-secret \
+    --from-file=rabbitmq.conf=rabbitmq.conf \
+    --from-file=definitions.json=definitions.json \
+    --from-file=cacert.pem=cacert.pem \
+    --from-file=cert.pem=cert.pem \
+    --from-file=key.pem=key.pem
+
+# kubectl create -f rabbitmq.yaml
+
+# echo "waiting for rabbitmq"
+
+# while ! kubectl exec svc/rabbitmq -- rabbitmqctl await_startup --timeout 300; do
+#     sleep 1
+# done
+
+# setup_rabbitmq_user "admin" "admin" ".*" ".*" ".*"
+# setup_rabbitmq_user "service" "service" "^$" ".*" ".*"
+
+# # TODO extract config management as we get there
 # kubectl create secret generic rabbitmq-service-secret \
 #     --from-literal=RABBITMQ_SERVICE_USER=service \
-#     --from-literal=RABBITMQ_SERVICE_PASSWORD=secret
+#     --from-literal=RABBITMQ_SERVICE_PASSWORD=service
