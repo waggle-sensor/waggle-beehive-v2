91dce5afd9de5901d36592c613cbc8f4790bbfb1
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-02-09 14:39:20 -0600

    removed beehive-rabbitmq since we're just using rabbitmq image

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker build -t waggle/beehive-rabbitmq beehive-rabbitmq && docker push waggle/beehive-rabbitmq
 docker build -t waggle/beehive-data-logger beehive-data-logger && docker push waggle/beehive-data-logger
