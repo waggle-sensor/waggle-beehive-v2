1c16637c4c37d7c14511fd65857d66e86e992181
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-02-11 15:15:41 -0600

    added example of integrating influxdb and data loader for quick graphs

--- Vagrantfile
+++ Vagrantfile
@@ -6,6 +6,7 @@ Vagrant.configure("2") do |config|
 
   config.vm.hostname = "beehive"
   config.vm.network "private_network", ip: "10.31.81.200"
+  config.vm.network "forwarded_port", guest: 8086, host: 8086
 
   config.vm.provider "virtualbox" do |vb|
     vb.memory = "2048"
--- services/beehive-influxdb-loader/main.py
+++ services/beehive-influxdb-loader/main.py
@@ -1,11 +1,11 @@
 import argparse
 import pika
-import waggle.message as message
 import influxdb_client
-from influxdb_client.client.write_api import SYNCHRONOUS
-from prometheus_client import start_http_server, Counter
+from influxdb_client.client.write_api import SYNCHRONOUS, WritePrecision
 from os import getenv
 import logging
+import ssl
+import waggle.message as message
 
 
 def main():
@@ -13,15 +13,18 @@ def main():
     parser.add_argument("--debug", action="store_true")
     parser.add_argument("--rabbitmq_host",default=getenv("RABBITMQ_HOST", "localhost"))
     parser.add_argument("--rabbitmq_port", default=getenv("RABBITMQ_PORT", "5672"), type=int)
-    parser.add_argument("--rabbitmq_username", default=getenv("RABBITMQ_USERNAME", "guest"))
-    parser.add_argument("--rabbitmq_password", default=getenv("RABBITMQ_PASSWORD", "guest"))
-    parser.add_argument("--rabbitmq_ssl_cacertfile", default=getenv("RABBITMQ_SSL_CACERTFILE"))
+    parser.add_argument("--rabbitmq_username", default=getenv("RABBITMQ_USERNAME", ""))
+    parser.add_argument("--rabbitmq_password", default=getenv("RABBITMQ_PASSWORD", ""))
+    parser.add_argument("--rabbitmq_cacertfile", default=getenv("RABBITMQ_CACERTFILE", ""))
+    parser.add_argument("--rabbitmq_certfile", default=getenv("RABBITMQ_CERTFILE", ""))
+    parser.add_argument("--rabbitmq_keyfile", default=getenv("RABBITMQ_KEYFILE", ""))
     parser.add_argument("--rabbitmq_exchange", default=getenv("RABBITMQ_EXCHANGE", "waggle.msg"))
-    parser.add_argument("--rabbitmq_queue", default=getenv("RABBITMQ_QUEUE", "influxdb-messages"))
+    parser.add_argument("--rabbitmq_queue", default=getenv("RABBITMQ_QUEUE", "influx-messages"))
     parser.add_argument("--influxdb_url", default=getenv("INFLUXDB_URL", "http://localhost:8086"))
     parser.add_argument("--influxdb_token", default=getenv("INFLUXDB_TOKEN"))
     parser.add_argument("--influxdb_bucket", default=getenv("INFLUXDB_BUCKET", "waggle"))
     parser.add_argument("--influxdb_org", default=getenv("INFLUXDB_ORG", "waggle"))
+    
     args = parser.parse_args()
 
     logging.basicConfig(
@@ -30,11 +33,6 @@ def main():
         datefmt="%Y/%m/%d %H:%M:%S")
     # pika logging is too verbose, so we turn it down.
     logging.getLogger("pika").setLevel(logging.CRITICAL)
-
-    # setup and run metrics for prometheus monitoring
-    waggle_messages_total = Counter("waggle_messages_total", "Total messages for a node and plugin", ["node", "plugin"])
-    waggle_errors_total = Counter("waggle_errors_total", "Total number of errors")
-    start_http_server(9123)
 
     logging.info("connecting to influxdb at %s", args.influxdb_url)
     client = influxdb_client.InfluxDBClient(
@@ -51,26 +49,46 @@ def main():
         except Exception:
             ch.basic_ack(method.delivery_tag)
             logging.warning("failed to parse message")
-            waggle_errors_total.inc()
             return
 
         try:
             record = (influxdb_client.Point(msg.name)
                 .tag("node", msg.meta["node"])
                 .tag("plugin", msg.meta["plugin"])
-                .field("value", msg.value))
+                .field("value", msg.value)
+                .time(msg.timestamp, WritePrecision.NS))
         except KeyError as key:
             ch.basic_ack(method.delivery_tag)
             logging.warning("message missing meta %s", key)
-            waggle_errors_total.inc()
             return
 
         writer.write(bucket=args.influxdb_bucket, org=args.influxdb_org, record=record)
         ch.basic_ack(method.delivery_tag)
         logging.debug("proccessed message %s", msg)
-        waggle_messages_total.labels(node=msg.meta["node"], plugin=msg.meta["plugin"]).inc()
 
-    params = pika.URLParameters(args.rabbitmq_url)
+    if args.rabbitmq_username != "":
+        credentials = pika.PlainCredentials(args.rabbitmq_username, args.rabbitmq_password)
+    else:
+        credentials = pika.credentials.ExternalCredentials()
+
+    if args.rabbitmq_cacertfile != "":
+        context = ssl.create_default_context(cafile=args.rabbitmq_cacertfile)
+        # HACK this allows the host and baked in host to be configured independently
+        context.check_hostname = False
+        if args.rabbitmq_certfile != "":
+            context.load_cert_chain(args.rabbitmq_certfile, args.rabbitmq_keyfile)
+        ssl_options = pika.SSLOptions(context, args.rabbitmq_host)
+    else:
+        ssl_options = None
+
+    params = pika.ConnectionParameters(
+        host=args.rabbitmq_host,
+        port=args.rabbitmq_port,
+        credentials=credentials,
+        ssl_options=ssl_options,
+        retry_delay=60,
+        socket_timeout=10.0)
+
     conn = pika.BlockingConnection(params)
     ch = conn.channel()
     ch.queue_declare(args.rabbitmq_queue, durable=True)
