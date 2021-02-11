8126b75108c343943a1bfdfc63f2b3237b349d7b
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-02-11 14:07:01 -0600

    using fixed csr.conf. only create tls and ssh ca if they don't exist.

--- pki-tools/create-ca.sh
+++ pki-tools/create-ca.sh
@@ -1,34 +1,21 @@
 #!/bin/bash -e
 
-# ensure working in pki-tools dir
 cd $(dirname $0)
 
 CN=beekeeper
 
-# generate signing config
-cat <<EOF > csr.conf
-[ req ]
-default_bits = 2048
-prompt = no
-default_md = sha256
-distinguished_name = dn
+echo "generating tls ca"
+if test -e cakey.pem; then
+    echo "$PWD/cakey.pem already exists - skipping."
+else
+    openssl genrsa -out cakey.pem 2048
+    chmod 600 cakey.pem
+    openssl req -x509 -new -nodes -key cakey.pem -subj "/CN=${CN}" -days 3650 -out cacert.pem
+fi
 
-[ dn ]
-C = US
-CN = ${CN}
-
-[ v3_ext ]
-authorityKeyIdentifier=keyid,issuer:always
-basicConstraints=CA:FALSE
-keyUsage=keyEncipherment,dataEncipherment
-extendedKeyUsage=serverAuth,clientAuth
-EOF
-
-# generate tls ca key and cert
-openssl genrsa -out cakey.pem 2048
-openssl req -x509 -new -nodes -key cakey.pem -subj "/CN=${CN}" -days 3650 -out cacert.pem
-chmod 600 cakey.pem
-
-# genrate ssh ca
-rm -f ca
-ssh-keygen -C "${CN}" -N "" -f ca
+echo "generating ssh ca"
+if test -e ca; then
+    echo "$PWD/ca already exists - skipping."
+else
+    ssh-keygen -C "${CN}" -N "" -f ca
+fi
