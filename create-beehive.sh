aebb510b72e6c29c68bc6b2483d86f8fdb737353
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-02-23 14:42:15 -0600

    adding influxdb, data loader and data api deploy steps

--- create-beehive.sh
+++ create-beehive.sh
@@ -26,6 +26,52 @@ echo "deploying upload server"
 pki-tools/create-and-sign-ssh-host-key-secret.sh beehive-upload-server upload-server-ssh-host-key-secret
 kubectl apply -f kubernetes/upload-server.yaml
 
+echo "deploying influxdb"
+kubectl apply -f kubernetes/influxdb.yaml
+
+setup_influxdb() {
+    kubectl exec svc/influxdb -- influx setup \
+        --org waggle \
+        --bucket waggle \
+        --username waggle \
+        --password wagglewaggle \
+        --force
+}
+
+echo "setting up influxdb"
+while true; do
+    if msg=$(setup_influxdb); then
+        break
+    fi
+    if [[ "$msg" == *"already been setup"* ]]; then
+        echo "influxdb already setup. skipping."
+        break
+    fi
+    echo "influxdb not ready... will retry."
+    sleep 3
+done
+
+generate_influxdb_token() {
+    # token is emitted in second column
+    kubectl exec svc/influxdb -- influx auth create \
+        --user waggle \
+        --org waggle \
+        --hide-headers $* | awk '{print $2}'
+}
+
+echo "generating token for data loader"
+token=$(generate_influxdb_token --write-buckets)
+kubectl create secret generic influxdb-loader-secret \
+    --from-literal=token="$token"
+pki-tools/create-and-sign-tls-secret.sh influxdb-loader influxdb-loader-tls-secret
+kubectl apply -f kubernetes/influxdb-loader.yaml
+
+echo "generating token for data api"
+token=$(generate_influxdb_token --read-buckets)
+kubectl create secret generic influxdb-data-api-secret \
+    --from-literal=token="$token"
+kubectl apply -f kubernetes/influxdb-data-api.yaml
+
 echo "creating credentials for but will not deploy message generator"
 pki-tools/create-and-sign-tls-secret.sh message-generator message-generator-tls-secret
 # kubectl apply -f kubernetes/message-generator.yaml
