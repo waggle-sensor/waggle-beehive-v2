c95ad7ffb8997fc91f2fcf4b01f8dad496b18da2
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-03-08 10:28:30 -0600

    fixed svc name

--- add-nodes-to-upload-server.sh
+++ add-nodes-to-upload-server.sh
@@ -3,7 +3,7 @@
 for nodeID in $*; do
     username="node-$nodeID"
 
-    kubectl exec --stdin deployment/upload-server -- sh -s <<EOF
+    kubectl exec --stdin deployment/beehive-upload-server -- sh -s <<EOF
 adduser -D -g "" "$username"
 passwd -u "$username"
 true
